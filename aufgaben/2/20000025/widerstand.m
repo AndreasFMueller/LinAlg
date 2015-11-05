0bb093b27c0ff54824c67e757f14a0cb00985997
Andreas Mueller <andreas.mueller@othello.ch>
2015-11-05 05:26:11 +0100

    endgueltige Fassung der Loesung zur Wettbewerbsaufgabe

--- aufgaben/2/20000025/widerstand.m
+++ aufgaben/2/20000025/widerstand.m
@@ -1,7 +1,26 @@
 source("randoperator.m");
+
 source("kubooktaeder.m");
+source("rhombendodekaeder.m");
+
+d = rand(rhombendodekaeder)
+z = zyklen(d)
+R = eye(size(z)(2))
+[A, b] = kirchhoff(z, R, d)
+I = A \ b
+
+i1 = I(size(I)(1))
+
 d = rand(kubooktaeder)
 z = zyklen(d)
 R = eye(size(z)(2))
-A = kirchhoff(z, R, d)
+[A, b] = kirchhoff(z, R, d)
+I = A \ b
+
+i2 = I(size(I)(1))
+
+format long e
+i1
+i2
+i1 + i2
 
